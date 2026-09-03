e8708ab101dc8a6de5d280c27100e4e230e822f6
Ramsingh Kushwaha <141406461+Ramsingh45156@users.noreply.github.com>
2026-09-03 18:04:42 +0530

    Time: 0 ms (100%), Space: 165.9 MB (62.83%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int mn = INT_MAX, oddCnt=0;
+        for(int x : nums1) {
+            mn = min(mn,x);
+            if(x%2==1) oddCnt++;
+        }
+        return mn % 2 || oddCnt == 0;
+    }
+};
